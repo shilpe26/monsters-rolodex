d3c1b889dba30e12f0f19145c4511be7feb15d44
Shilpe Saxena <shilpecsaxena9098@gmail.com>
2020-05-08 12:45:30 +0530

    Updates

---
+++ precache-manifest.471b1278d69e13df7a2baeacb4406fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89add97180d4da94e3e41313dab2d36",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "265a97eef724dbda1c5d",
+    "url": "/monster-rolodex/static/css/main.88a3b8f6.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265a97eef724dbda1c5d",
+    "url": "/monster-rolodex/static/js/main.365eaa49.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
